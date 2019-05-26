fc847d3bda6b95463362b30c30ab044c4c6e70b6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-26 09:01:17 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,18 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def create
+
+  end
 end
