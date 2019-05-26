b242b6c1e115fd9f53caf0c1f5a89ce4a33929eb
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-26 09:20:53 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,9 +15,8 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    if @game.save
-
-    end
+    @game.save
+    render json: @game,status:201
   end
 
   private
